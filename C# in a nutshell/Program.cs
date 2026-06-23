55fdf14aef43401b63e27292b386b5a081d9c0bb
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-06-23 22:47:33 +0400

    little work

--- C# in a nutshell/Program.cs
+++ C# in a nutshell/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");class Person (string firstName, string lastName)
+{
+public void Print() => Console.WriteLine (firstName + " " + lastName);
+}
